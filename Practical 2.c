6c45d8ceade8eb3b4c9b62df79f1f7278ae34dfa
24CS054KRISH <24cs054@charusat.edu.in>
2024-10-05 14:45:17 +0530

    practical 2

---
+++ Practical 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int l,b;
+    printf("Entre the value of l(length) :\n");
+    scanf("%d",&l);
+
+    printf("Entre the value of b(breath) :\n");
+    scanf("%d",&b);
+
+    printf("Area of Rectangle is = %d\n",l*b);
+    printf("Peremetre of Rectangle is = %d\n",2*(l+b));
+
+    printf("Thank You");
+    return 0;
+}
